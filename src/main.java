4127a3f3c57f07f6ab0e5ab35282c36d77f7d821
eeoviosa <116686424+eeoviosa@users.noreply.github.com>
2024-03-11 16:43:18 -0500

    first commmit

--- src/main.java
+++ src/main.java
@@ -19,7 +19,7 @@ public class main implements ActionListener{
 	
 	main()
 	{
-		frame = new JFrame("Calculator");
+		frame = new JFrame("Simple Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420,550);
 		frame.setLayout(null);
