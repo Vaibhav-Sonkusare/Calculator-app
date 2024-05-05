1e0f219205f713ea1a59da10565177cc03996055
vaibhav <vaibhavsonkusare2@gmail.com>
2024-05-05 12:46:51 +0530

    corrected indentation errors in src/main.java

--- src/main.java
+++ src/main.java
@@ -38,61 +38,59 @@ public class main implements ActionListener{
 		mulButton = new JButton("*");
 		negButton = new JButton("(-)");
 		
-    functionButtons[0] = addButton;
-    functionButtons[1] = divButton;
-    functionButtons[2] = mulButton;
-    functionButtons[3] = clrButton;
-    functionButtons[4] = subButton;
-    functionButtons[5] = decButton;
-    functionButtons[6] = delButton;
-    functionButtons[7] = equButton;
-    functionButtons[8] = negButton;
+        functionButtons[0] = addButton;
+        functionButtons[1] = divButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = clrButton;
+        functionButtons[4] = subButton;
+        functionButtons[5] = decButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = equButton;
+        functionButtons[8] = negButton;
+        
+        for(int i = 0; i < 9; i++)
+        {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+            
+        }
+        
+        for(int i = 0; i < 10; i++)
+        {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+            
+        }
+        
+        negButton.setBounds(50,430,100,50);
+        delButton.setBounds(150,430,100,50);
+        clrButton.setBounds(250,430,100,50);
+        
+        panel = new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+        
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
     
-    for(int i = 0; i < 9; i++)
-    {
-    	functionButtons[i].addActionListener(this);
-    	functionButtons[i].setFont(myFont);
-    	functionButtons[i].setFocusable(false);
-    	
-    }
-    
-    for(int i = 0; i < 10; i++)
-    {
-    	numberButtons[i] = new JButton(String.valueOf(i));
-    	numberButtons[i].addActionListener(this);
-    	numberButtons[i].setFont(myFont);
-    	numberButtons[i].setFocusable(false);
-    	
-    }
-    
-    negButton.setBounds(50,430,100,50);
-    delButton.setBounds(150,430,100,50);
-    clrButton.setBounds(250,430,100,50);
-    
-    panel = new JPanel();
-    panel.setBounds(50,100,300,300);
-    panel.setLayout(new GridLayout(4,4,10,10));
-    
-    panel.add(numberButtons[1]);
-    panel.add(numberButtons[2]);
-    panel.add(numberButtons[3]);
-    panel.add(addButton);
-    panel.add(numberButtons[4]);
-    panel.add(numberButtons[5]);
-    panel.add(numberButtons[6]);
-    panel.add(subButton);
-    panel.add(numberButtons[7]);
-    panel.add(numberButtons[8]);
-    panel.add(numberButtons[9]);
-    panel.add(mulButton);
-    panel.add(decButton);
-    panel.add(numberButtons[0]);
-    panel.add(equButton);
-    panel.add(divButton);
-    
-    
-    
-    
+
 		
 		frame.add(panel);
 		frame.add(clrButton);
@@ -103,7 +101,7 @@ public class main implements ActionListener{
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-main calc = new main();
+        main calc = new main();
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
